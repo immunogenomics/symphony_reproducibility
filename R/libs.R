be3c063834b9e1105f366d203228cbeafcfbfe30
joycekang <joyce_kang@hms.harvard.edu>
2021-06-24 17:10:46 -0400

    added scATAC

--- R/libs.R
+++ R/libs.R
@@ -1,31 +1,34 @@
 library(harmony)
 library(tidyverse)
 library(data.table)
-library(pheatmap)
 library(matrixStats)
-library(patchwork)
-library(ggthemes)
 library(Matrix)
 library(uwot)
-library(ggplot2)
 library(umap)
 library(plyr)
 library(dplyr)
 library(singlecellmethods)
+library(lisi)
 library(Rcpp)
 library(irlba)
-library(class) # for knn1
+library(class) # for knn
 library(presto)
 library(RANN) # for nn
 library(reshape2)
 library(stats) # for distance
 library(pdist) # for Euc dist between 2 matrices
 library(purrr) # for mapreduce
-#library(hdf5r) # for reading in Seurat objects
+#library(GGally) # for ggpairs
+library(Seurat)
+library(symphony)
+library(biomaRt)
+
+# For plotting
+library(pheatmap)
+library(RColorBrewer)
 library(ggrastr)
 library(ggpubr)
 library(ggrepel)
-library(Seurat)
-library(symphony)
-library(RColorBrewer)
-library(biomaRt)
+library(ggplot2)
+library(patchwork)
+library(ggthemes)
